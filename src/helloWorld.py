3baf7496953c699ab2a96ac993d5a2f678043c06
Xfk <ca2pa23187@gmail.com>
2020-02-28 11:50:47 +0800

    latest

---
+++ src/helloWorld.py
@@ -0,0 +1,15 @@
+import random
+import xfk.ok as ok
+from xfk import invoke
+
+num = random.random()
+
+
+# def test(num):
+#     print(__name__)
+#     print(num)
+
+
+if __name__ == "__main__":
+    ok.test()
+    invoke.test()
